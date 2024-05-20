4c56a25cfde85122c123dac1d871c0702de9d56d
abdums <ace272@icloud.com>
2024-05-19 22:06:36 -0400

    Start Tiles

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const tilesContainer = document.querySelector(".tiles");
+const colors = [
+  "aqua",
+  "aquamarine",
+  "crimson",
+  "blue",
+  "dodgerblue",
+  "gold",
+  "greenyellow",
+  "teal",
+];
+const colorPickList = [...colors, ...colors];
+console.log(colorPickList);
